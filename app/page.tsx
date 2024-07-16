527e4b7aef6553b687830425faa932670c877872
nahuelsoaje <soajenahuel9@gmail.com>
2024-07-15 22:04:44 -0300

    agregue card 3d en una nueva seccion de proyectos

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,21 @@ import Skills from "@/components/Skills";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { SparklesCore } from "@/components/ui/SparklesCore";
 import info  from "../utils/info"; // Asegúrate de tener la ruta correcta para tu archivo info.js
+import Proyectos2 from "@/components/ui/Proyectos2";
+
 
 export default function Home() {
   const {navItems} = info
 
   return (
-    <div className="relative min-h-screen overflow-hidden">
+    <div className="bg-black relative min-h-screen overflow-hidden">
       <main className="relative z-10 flex flex-col items-center justify-between gap-14 min-h-screen">
         <Hero />
         <FloatingNav navItems={navItems} />
         <div className="flex flex-col items-center justify-between gap-14">
           <AboutMe />
           <Skills />
+          <Proyectos2/>
           <Projects />
           <Contact />
         </div>
